ef4577d602da503aac061865a9dfd68932a0676f
reymanov <kubarejmann@wp.pl>
2022-06-22 20:15:22 +0200

    Kalkulator baza

--- Aplikacje.NET/MainWindow.xaml.cs
+++ Aplikacje.NET/MainWindow.xaml.cs
@@ -15,12 +15,72 @@ using System.Windows.Shapes;
 
 namespace Aplikacje.NET
 {
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
-
+        Model model = new Model();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = model;
+        }
+
+        private void Cyfra(object sender, RoutedEventArgs e)
+        {
+            model.DopiszCyfrę(
+                (string)((Button)sender).Content
+                );
+        }
+
+        private void ZmianaZnaku(object sender, RoutedEventArgs e)
+        {
+            model.ZmieńZnak();
+        }
+
+        private void Przecinek(object sender, RoutedEventArgs e)
+        {
+            model.PostawPrzecinek();
+        }
+
+        private void Cofnij(object sender, RoutedEventArgs e)
+        {
+            model.Cofnij();
+        }
+
+        private void Resetuj(object sender, RoutedEventArgs e)
+        {
+            model.Resetuj();
+        }
+
+        private void Zeruj(object sender, RoutedEventArgs e)
+        {
+            model.Zeruj();
+        }
+
+        private void DziałanieZwykłe(object sender, RoutedEventArgs e)
+        {
+            model.DziałanieZwykłe(
+                ((Button)sender).Content.ToString()
+                );
+        }
+
+        private void PodajWynik(object sender, RoutedEventArgs e)
+        {
+            model.PodajWynik();
+        }
+
+        private void DziałanieJednoargumentowe(object sender, RoutedEventArgs e)
+        {
+            model.DziałanieJednoargumentowe(
+                ((Button)sender).Content.ToString()
+                );
+        }
+
+        private void DziałanieProcentowe(object sender, RoutedEventArgs e)
+        {
+            model.Procent();
         }
     }
 }
---
+++ Aplikacje.NET/Model.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacje.NET
+{
+    class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        double
+            liczbaA,
+            liczbaB
+            ;
+        string buforDziałania = null;
+        bool
+            flagaUłamka = false,
+            flagaDziałania = false
+            ;
+        string buforIO = "0";
+        public string IO
+        {
+            get { return buforIO; }
+            set
+            {
+                buforIO = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IO"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Bufory"));
+            }
+        }
+
+        public double LiczbaA
+        {
+            get => liczbaA;
+            set
+            {
+                liczbaA = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Bufory"));
+            }
+        }
+        public double LiczbaB
+        {
+            get => liczbaB;
+            set
+            {
+                liczbaB = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Bufory"));
+            }
+        }
+        public string BuforDziałania
+        {
+            get => buforDziałania;
+            set
+            {
+                buforDziałania = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Bufory"));
+            }
+        }
+        public string Bufory
+        {
+            get
+            {
+                if (buforDziałania == null)
+                    return "";
+                if (flagaDziałania == false)
+                    return $"{liczbaA} {buforDziałania}";
+                return $"{liczbaA} {buforDziałania} {liczbaB}";
+            }
+        }
+
+        internal void DziałanieZwykłe(string znak)
+        {
+            if (flagaDziałania == true)
+                ;
+            else if (BuforDziałania == null)
+            {
+                BuforDziałania = znak;
+                LiczbaA = double.Parse(buforIO);
+                flagaDziałania = true;
+            }
+            else
+            {
+                BuforDziałania = znak;
+                LiczbaB = double.Parse(buforIO);
+                flagaDziałania = true;
+                LiczbaA = WykonajDziałanie();
+                IO = LiczbaA.ToString();
+            }
+        }
+
+        internal void Procent()
+        {
+            flagaDziałania = true;
+            LiczbaB = double.Parse(buforIO) / 100 * liczbaA;
+            PodajWynik();
+        }
+
+        internal void DziałanieJednoargumentowe(string działanie)
+        {
+            BuforDziałania = działanie;
+            flagaDziałania = true;
+            IO = WykonajDziałanie().ToString();
+        }
+
+        internal void PodajWynik()
+        {
+            if (flagaDziałania == false)
+            {
+                LiczbaB = double.Parse(buforIO);
+                flagaDziałania = true;
+            }
+            IO = WykonajDziałanie().ToString();
+            liczbaA = double.Parse(IO);
+        }
+
+        private double WykonajDziałanie()
+        {
+            if (BuforDziałania == "+")
+                return LiczbaA + LiczbaB;
+            else if (BuforDziałania == "x²")
+                return LiczbaA * LiczbaA;
+            else
+                return 0;
+        }
+
+        internal void Resetuj()
+        {
+            Zeruj();
+            BuforDziałania = default;
+            LiczbaA = default;
+            LiczbaB = default;
+        }
+        internal void Zeruj()
+        {
+            flagaUłamka = false;
+            flagaDziałania = false;
+            IO = "0";
+        }
+        internal void Cofnij()
+        {
+            if (buforIO == "0")
+                return;
+            else if (
+                buforIO == "0,"
+                ||
+                buforIO == "-0,"
+                ||
+                (buforIO[0] == '-' && buforIO.Length == 2)
+                )
+                Zeruj();
+            else
+            {
+                char usuwanyZnak = buforIO[buforIO.Length - 1];
+                IO = buforIO.Substring(0, buforIO.Length - 1);
+                if (usuwanyZnak == ',')
+                    flagaUłamka = false;
+            }
+        }
+        internal void DopiszCyfrę(string cyfra)
+        {
+            if (flagaDziałania)
+                Zeruj();
+            if (buforIO == "0")
+                buforIO = "";
+            IO += cyfra;
+        }
+        internal void ZmieńZnak()
+        {
+            flagaDziałania = false;
+            if (buforIO == "0")
+                return;
+            else if (buforIO[0] == '-')
+                IO = buforIO.Substring(1);
+            else
+                IO = '-' + IO;
+        }
+        internal void PostawPrzecinek()
+        {
+            if (flagaDziałania)
+                Zeruj();
+            if (flagaUłamka || buforIO[buforIO.Length - 1] == ',')
+                return;
+            else
+            {
+                IO += ',';
+                flagaUłamka = true;
+            }
+        }
+    }
+}
